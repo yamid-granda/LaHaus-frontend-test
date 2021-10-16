9a9f23a7234451d525e5f59b91d77e53618cd339
Yamid Granda <yamid.granda@gmail.com>
2021-10-16 12:16:18 -0500

    chore: customize tailwind spacing 49, 73 radius xl

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    borderRadius: {
+      'xl': '0.625em',
+    },
+    extend: {
+      spacing: {
+        '49': '12.25rem',
+        '73': '18.25rem',
+      },
+    }
+  }
+}
