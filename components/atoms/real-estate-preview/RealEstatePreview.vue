10b8f64f4caa4b3b42869559d199157c98c63ec0
Yamid Granda <yamid.granda@gmail.com>
2021-10-16 12:24:36 -0500

    feat: RealEstatePreview component

---
+++ components/atoms/real-estate-preview/RealEstatePreview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="lh-real-estate-preview">
+    <img class="lh-real-estate-preview__image" :src="image" :alt="name" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: { type: String, required: true },
+    name: { type: String, required: true },
+  },
+}
+</script>
+
+<style>
+.lh-real-estate-preview {
+  @apply rounded-xl h-49 w-73 overflow-hidden;
+}
+
+.lh-real-estate-preview__image {
+  @apply object-cover w-full h-full;
+}
+</style>
