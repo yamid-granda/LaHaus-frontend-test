1525e67c0b30a53ab209314c3f3caed2c9741152
Yamid Granda <yamid.granda@gmail.com>
2021-10-17 16:14:45 -0500

    chore: tailwind ui base extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,34 @@
+const colors = require('tailwindcss/colors')
+
+function parseSpacingToRem(spacing) {
+  const rem = (spacing * 4) / 16
+  return `${rem}rem`
+}
+
+function parseSpacingsToConfig(spacings) {
+  return spacings.reduce((config, spacing) => {
+    config[spacing] = parseSpacingToRem(spacing)
+    return config
+  }, {})
+}
+
+const extraSpacings = [15, 19, 21, 25, 33, 34, 37, 39, 49, 73]
+const spacingExtend = parseSpacingsToConfig(extraSpacings)
+
 module.exports = {
   theme: {
-    borderRadius: {
-      'xl': '0.625em',
-    },
     extend: {
-      spacing: {
-        '49': '12.25rem',
-        '73': '18.25rem',
+      colors: {
+        ...colors,
+        'lh-jungle-green-900': '#012D26',
+        'lh-green-gray-500': '#5B6866',
       },
-    }
-  }
+      spacing: {
+        ...spacingExtend,
+      },
+      borderRadius: {
+        xl: '0.625em',
+      },
+    },
+  },
 }
