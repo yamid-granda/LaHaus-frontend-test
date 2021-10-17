b8aabf90779162a9643e161b8a167a7c4616604e
Yamid Granda <yamid.granda@gmail.com>
2021-10-17 16:24:15 -0500

    feat: RealEstateCollections component

---
+++ components/molecules/index.js
@@ -0,0 +1,3 @@
+import _RealEstateCollections from './real-estate-collections/RealEstateCollections.vue';
+
+export const RealEstateCollections = _RealEstateCollections;
---
+++ components/molecules/real-estate-collections/RealEstateCollections.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="lh-real-estate-collections">
+    <div
+      v-for="{ id, realEstates, name } in collections"
+      :key="id"
+      class="lh-real-estate-collections__collection"
+    >
+      <RealEstateCollection :name="name" :real-estates="realEstates"></RealEstateCollection>
+    </div>
+  </div>
+</template>
+
+<script>
+import { RealEstateCollection } from '@/components'
+
+export default {
+  components: { RealEstateCollection },
+  props: { collections: { type: Array, default: () => [] } },
+}
+</script>
+
+<style lang="scss"></style>
