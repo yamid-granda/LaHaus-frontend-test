4691aa09fb2b278035f581e49b40a1307d415546
Yamid Granda <yamid.granda@gmail.com>
2021-10-17 16:04:39 -0500

    chore: eslint base config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,34 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    node: true,
+    es6: true,
+    jest: true,
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'prettier'
+    'plugin:vue/essential',
   ],
-  plugins: [
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-depth': ['warn', 2],
+    'arrow-parens': [2, 'as-needed'],
+    'import/prefer-default-export': 'off',
+    'no-await-in-loop': 'off',
+    'no-restricted-syntax': ['off', 'ForOfStatement'],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
   ],
-  // add your custom rules here
-  rules: {}
-}
+};
