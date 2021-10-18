db51b6b5321a150ab02d0cf3bd106a770ce6bd19
Yamid Granda <yamid.granda@gmail.com>
2021-10-17 21:13:34 -0500

    feat: FavoriteRealEstatesManageModule component

---
+++ components/organisms/favorite-real-estates-manage-module/FavoriteRealEstatesManageModule.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="lh-favorite-real-estates-manage-module">
+    <RealEstateCollections :collections="collections" />
+  </div>
+</template>
+
+<script>
+import { getFavoriteRealEstateCollections } from '@/utils'
+import { RealEstateCollections } from '@/components'
+
+export default {
+  components: { RealEstateCollections },
+  async fetch() {
+    this.collections = await getFavoriteRealEstateCollections()
+  },
+  data() {
+    return {
+      collections: [],
+    }
+  },
+}
+</script>
+
+<style lang="scss"></style>
---
+++ components/organisms/index.js
@@ -0,0 +1,3 @@
+import _FavoriteRealEstatesManageModule from './favorite-real-estates-manage-module/FavoriteRealEstatesManageModule.vue';
+
+export const FavoriteRealEstatesManageModule = _FavoriteRealEstatesManageModule;
