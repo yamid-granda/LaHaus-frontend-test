8b30beabaa8b4f3b8f98798f5c16991100620c7f
Yamid Granda <yamid.granda@gmail.com>
2021-10-17 21:11:50 -0500

    feat: real estate http service

---
+++ utils/services/http/real-estate/index.js
@@ -0,0 +1,42 @@
+import { httpGet } from '@/utils'
+
+function mapRealEstate(realEstate) {
+  const { gallery_urls: galleryUrls, name } = realEstate
+  const imageSrc = galleryUrls[0] || null
+  return {
+    // todo: map other real estate data
+    // ...realEstate,
+    name,
+    imageSrc,
+  }
+}
+
+function parseRealEstatesDataToDictionary(realEstatesData) {
+  return realEstatesData.reduce((dictionary, apiData) => {
+    const { id, attributes } = apiData
+    dictionary[id] = attributes
+    return dictionary
+  }, {})
+}
+
+function parseRealEstateCollectionsDataToCollections({ collectionsData, realEstatesDictionary }) {
+  return collectionsData.map(({ id, attributes }) => {
+    const { name, real_estate_ids: realEstateIds } = attributes
+    const realEstates = realEstateIds.map(realEstateId =>
+      mapRealEstate(realEstatesDictionary[realEstateId]),
+    )
+    return {
+      id,
+      name,
+      realEstates,
+    }
+  })
+}
+
+export async function getFavoriteRealEstateCollections() {
+  const response = await httpGet({ url: 'real-estates' })
+  if (!response) return null
+  const { data: collectionsData, included: realEstatesData } = response
+  const realEstatesDictionary = parseRealEstatesDataToDictionary(realEstatesData)
+  return parseRealEstateCollectionsDataToCollections({ collectionsData, realEstatesDictionary })
+}
