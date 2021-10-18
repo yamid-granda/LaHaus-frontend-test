c25330b9ce45a7018880a2a3802a08dd53b9e354
Yamid Granda <yamid.granda@gmail.com>
2021-10-17 22:08:51 -0500

    feat: AppContainer component

---
+++ components/atoms/app-container/AppContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="lh-app-container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.lh-app-container {
+  @apply px-6 lg:px-16;
+}
+</style>
--- components/atoms/index.js
+++ components/atoms/index.js
@@ -1,5 +1,7 @@
 import _RealEstateCollection from './real-estate-collection/RealEstateCollection.vue';
 import _RealEstatePreview from './real-estate-preview/RealEstatePreview.vue';
+import _AppContainer from './app-container/AppContainer.vue';
 
 export const RealEstateCollection = _RealEstateCollection;
 export const RealEstatePreview = _RealEstatePreview;
+export const AppContainer = _AppContainer;
