fb83bf056e20f85da5873ec485a3056ca4f07fe8
Yamid Granda <yamid.granda@gmail.com>
2021-10-17 22:09:16 -0500

    feat: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <AppContainer>
+      <Nuxt />
+    </AppContainer>
+  </div>
+</template>
+
+<script>
+import { AppContainer } from '@/components'
+
+export default {
+  components: { AppContainer },
+}
+</script>
+
+<style lang="scss"></style>
