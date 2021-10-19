332a3cc26b97b0fba2a67ce8f817773445b2d580
Yamid Granda <yamid.granda@gmail.com>
2021-10-18 23:29:08 -0500

    feat: H1 component

---
+++ components/atoms/h1/H1.vue
@@ -0,0 +1,12 @@
+<template functional>
+  <h1 class="lh-h1">
+    <slot />
+  </h1>
+</template>
+
+<style lang="scss">
+.lh-h1 {
+  @apply text-xl font-semibold
+    lg:text-3xl;
+}
+</style>
--- components/atoms/index.js
+++ components/atoms/index.js
@@ -2,8 +2,10 @@ import _RealEstateCollection from './real-estate-collection/RealEstateCollection
 import _RealEstatePreview from './real-estate-preview/RealEstatePreview.vue';
 import _AppContainer from './app-container/AppContainer.vue';
 import _Navbar from './navbar/Navbar.vue';
+import _H1 from './h1/H1.vue';
 
 export const RealEstateCollection = _RealEstateCollection;
 export const RealEstatePreview = _RealEstatePreview;
 export const AppContainer = _AppContainer;
 export const Navbar = _Navbar;
+export const H1 = _H1;
