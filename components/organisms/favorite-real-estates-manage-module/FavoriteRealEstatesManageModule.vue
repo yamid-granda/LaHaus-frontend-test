1fc88b42c29d2059d6791859ced6f2d5d6b7f3b5
Yamid Granda <yamid.granda@gmail.com>
2021-10-19 17:05:51 -0500

    feat: translate FavoriteRealEstatesManageModule component

--- components/organisms/favorite-real-estates-manage-module/FavoriteRealEstatesManageModule.vue
+++ components/organisms/favorite-real-estates-manage-module/FavoriteRealEstatesManageModule.vue
@@ -3,7 +3,7 @@
     <RealEstateCollections :collections="collections">
       <template #after-last-collection>
         <div class="lh-favorite-real-estates-manage-module__new">
-          <PreviewAction icon="+" message="Crear una nueva lista" />
+          <PreviewAction icon="+" :message="$translate('Create a new list')" />
         </div>
       </template>
     </RealEstateCollections>
@@ -13,11 +13,16 @@
 <script>
 import { getFavoriteRealEstateCollections } from '@/utils'
 import { RealEstateCollections, PreviewAction } from '@/components'
+import translateMixin from '@/mixins/translate'
 
 export default {
+  mixins: [translateMixin],
   components: { RealEstateCollections, PreviewAction },
   async fetch() {
     this.collections = await getFavoriteRealEstateCollections()
+  },
+  props: {
+    $translations: { type: Object, default: () => ({}) },
   },
   data() {
     return {
