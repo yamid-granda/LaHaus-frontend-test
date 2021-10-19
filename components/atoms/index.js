f213f6c5e9c407251942b15871a8d22cac75becc
Yamid Granda <yamid.granda@gmail.com>
2021-10-18 23:30:01 -0500

    feat: PreviewAction component

--- components/atoms/index.js
+++ components/atoms/index.js
@@ -1,11 +1,13 @@
 import _RealEstateCollection from './real-estate-collection/RealEstateCollection.vue';
 import _RealEstatePreview from './real-estate-preview/RealEstatePreview.vue';
 import _AppContainer from './app-container/AppContainer.vue';
+import _PreviewAction from './preview-action/PreviewAction.vue';
 import _Navbar from './navbar/Navbar.vue';
 import _H1 from './h1/H1.vue';
 
 export const RealEstateCollection = _RealEstateCollection;
 export const RealEstatePreview = _RealEstatePreview;
 export const AppContainer = _AppContainer;
+export const PreviewAction = _PreviewAction;
 export const Navbar = _Navbar;
 export const H1 = _H1;
---
+++ components/atoms/preview-action/PreviewAction.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="lh-preview-action">
+    <div class="lh-preview-action__preview">{{ icon }}</div>
+    <div class="lh-preview-action__info">
+      <slot name="info">{{ message }}</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: { type: String, default: '' },
+    icon: { type: String, default: '' },
+  },
+}
+</script>
+
+<style lang="scss">
+.lh-preview-action {
+  @apply text-lh-sky-blue-500 cursor-pointer;
+}
+
+.lh-preview-action__preview {
+  @apply bg-lh-blue-100 flex justify-center rounded-xl items-center h-49  text-6xl;
+}
+
+.lh-preview-action__info {
+  @apply text-center pt-5 text-lg font-medium;
+}
+</style>
