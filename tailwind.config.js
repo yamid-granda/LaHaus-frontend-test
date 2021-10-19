692288447911087563f1145333a8db3ea485095e
Yamid Granda <yamid.granda@gmail.com>
2021-10-18 23:23:20 -0500

    chore: tailwind spacing 18, white, blue, gray colors, text xl 22px

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,22 +12,28 @@ function parseSpacingsToConfig(spacings) {
   }, {})
 }
 
-const extraSpacings = [15, 19, 21, 25, 33, 34, 37, 39, 49, 73]
+const extraSpacings = [15, 18, 19, 21, 25, 33, 34, 37, 39, 49, 73]
 const spacingExtend = parseSpacingsToConfig(extraSpacings)
 
 module.exports = {
   theme: {
     extend: {
       colors: {
-        ...colors,
+        white: colors.white,
+        'lh-blue-100': '#EBEDFF',
+        'lh-sky-blue-500': '#3948FF',
         'lh-jungle-green-900': '#012D26',
         'lh-green-gray-500': '#5B6866',
+        'lh-gray-100': '#E5F0EE',
       },
       spacing: {
         ...spacingExtend,
       },
       borderRadius: {
-        xl: '0.625em',
+        xl: '0.625rem',
+      },
+      text: {
+        xl: '1.375rem',
       },
     },
   },
