3aa4806f50fa2769256a5cd7d1e45c4a979ff41c
Yamid Granda <yamid.granda@gmail.com>
2021-10-19 00:13:37 -0500

    feat: index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <Tutorial/>
+  <div class="lh-favorite-real-estates-page">
+    <div class="lh-favorite-real-estates-page__h1">
+      <H1>{{ $t('Favorites list') }}</H1>
+    </div>
+    <FavoriteRealEstatesManageModule />
+  </div>
 </template>
 
 <script>
-export default {}
+import { FavoriteRealEstatesManageModule, H1 } from '@/components'
+
+export default {
+  components: {
+    H1,
+    FavoriteRealEstatesManageModule,
+  },
+}
 </script>
+
+<style lang="scss">
+.lh-favorite-real-estates-page__h1 {
+  @apply text-center mb-14 text-lh-jungle-green-900
+    lg:mt-7;
+}
+</style>
