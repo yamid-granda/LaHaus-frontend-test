a6ed4dd40a05141be059f557704f80054dadf35c
Yamid Granda <yamid.granda@gmail.com>
2021-10-19 17:03:58 -0500

    chore: favorite real estates page translations

--- locales/en-US.js
+++ locales/en-US.js
@@ -1,3 +1,7 @@
 export default {
-  'Favorites list': 'Favorites list'
+  'Favorites list': 'Favorites list',
+  'My Profile': 'My Profile',
+  'saved properties': 'saved property | saved properties',
+  'Create a new list': 'Create a new list',
+  'Resources': 'Resources',
 }
--- locales/es-CO.js
+++ locales/es-CO.js
@@ -1,3 +1,7 @@
 export default {
-  'Favorites list': 'Listas de favoritos'
+  'Favorites list': 'Listas de favoritos',
+  'My Profile': 'Mi Perfil',
+  'saved properties': 'propiedad guardada | propiedades guardadas',
+  'Create a new list': 'Crear una nueva lista',
+  'Resources': 'Recursos',
 }
